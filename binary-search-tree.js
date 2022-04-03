e840cd025131574def389dab35d94c94cafaf059
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-04-02 21:38:47 -0600

    exercise complete

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -81,11 +81,12 @@ class BinarySearchTree {
     while (stack.length) {
       let current = stack.pop();
       if (current.val === val) return current;
-      if (current.right) {
-        stack.push(current.right);
-      }
-      if (current.left) {
+      if (val < current.val) {
+        if (!current.left) return undefined;
         stack.push(current.left);
+      } else {
+        if (!current.right) return undefined;
+        stack.push(current.right);
       }
     }
     return undefined;
@@ -94,22 +95,62 @@ class BinarySearchTree {
   /** findRecursively(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses recursion. */
 
-  findRecursively(val) {}
+  findRecursively(val) {
+    if (!this.root) return undefined;
+    function findVal(node) {
+      if (node.val === val) return node;
+      if (val < node.val) {
+        if (!node.left) return undefined;
+        return findVal(node.left);
+      } else {
+        if (!node.right) return undefined;
+        return findVal(node.right);
+      }
+    }
+    return findVal(this.root);
+  }
 
   /** dfsPreOrder(): Traverse the array using pre-order DFS.
    * Return an array of visited nodes. */
 
-  dfsPreOrder() {}
+  dfsPreOrder() {
+    let nodes = [];
+    function traverse(node) {
+      nodes.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return nodes;
+  }
 
   /** dfsInOrder(): Traverse the array using in-order DFS.
    * Return an array of visited nodes. */
 
-  dfsInOrder() {}
+  dfsInOrder() {
+    let nodes = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      nodes.push(node.val);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return nodes;
+  }
 
   /** dfsPostOrder(): Traverse the array using post-order DFS.
    * Return an array of visited nodes. */
 
-  dfsPostOrder() {}
+  dfsPostOrder() {
+    let nodes = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      nodes.push(node.val);
+    }
+    traverse(this.root);
+    return nodes;
+  }
 
   /** bfs(): Traverse the array using BFS.
    * Return an array of visited nodes. */
